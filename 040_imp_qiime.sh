d9456d2cac7e0d95da723c9247f6d494bf25f22f
Paul <paul.czechowski@abiori.org>
2018-01-11 19:01:38 -0500

    Repo init, README, started running script 40 with newly created manifest file

---
+++ 040_imp_qiime.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# 11.01.2018 - Paul Czechowski - paul.czechowski@gmail.com 
+# ========================================================
+# Wrapper for Qiime2 import script, manifest file must be available.
+# More info at https://docs.qiime2.org/2017.10/tutorials/importing/
+
+# For debugging only
+# ------------------ 
+set -x
+
+# Paths needs to change for remote execution, and executable paths have to be
+# ---------------------------------------------------------------------------
+# adjusted depending on machine location.
+# ----------------------------------------
+if [[ "$HOSTNAME" != "pc683.eeb.cornell.edu" ]]; then
+    printf "Execution on remote...\n"
+    trpth="/data/CU_inter_intra"
+    inpth='Zenodo/Manifest/05_manifest_remote.txt'
+elif [[ "$HOSTNAME" == "pc683.eeb.cornell.edu" ]]; then
+    printf "Setting qiime alias, execution on local...\n"
+    trpth="$(dirname "$PWD")"
+    qiime2cli() { qiime "$@"; }
+    inpth='Zenodo/Manifest/05_manifest_local.txt'
+fi
+
+otpth='Zenodo/Qiime/040_18S_paired-end-import.qza'
+
+
+# Run import script
+# -----------------------------
+qiime2cli tools import \
+  --type 'SampleData[PairedEndSequencesWithQuality]' \
+  --input-path  "$trpth"/"$inpth" \
+  --output-path "$trpth"/"$otpth" \
+  --source-format PairedEndFastqManifestPhred33
+
---
+++ README.md
@@ -0,0 +1,22 @@
+# Analysing 18S data from Pearl Harbour 
+
+First look at sequence data arriving 11.01.2018. Needed to see if approach is successful.
+If so, 18S data can be generated on large scale. This data also needs to be combined
+with the old sequence data. This folder was created 11.01.2018 and last updated
+11.01.2018. The GitHub folder is version tracked.
+
+## Progress notes
+
+*  **11.01.2017** - starting data analysis
+   *  created Qiime2 manifest file (which also point to location of raw data).
+   *  working with Qiime2 2017.12 for `cutadapt` functionality.
+   *  starting with script `040_imp_qiime.sh`
+   *  creating `.git` repository in script folder
+
+## Todo
+* adjust trasnsport script and pathnames for cluster operation.
+
+
+## Relevant Repository contents:
+
+[not yet required]
