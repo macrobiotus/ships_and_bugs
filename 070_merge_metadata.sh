a4780239b7a61494f358af93984b5c36edc9130c
Paul <paul.czechowski@abiori.org>
2019-03-28 16:50:41 -0400

    adjusting, committing and running `/Users/paul/Documents/CU_combined/Github/070_merge_metadata.sh` added hash values

--- 070_merge_metadata.sh
+++ 070_merge_metadata.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# 21.03.2019 - Paul Czechowski - paul.czechowski@gmail.com 
+# 28.03.2019 - Paul Czechowski - paul.czechowski@gmail.com 
 # ========================================================
 # merging metadata
 
@@ -25,12 +25,12 @@ fi
 # ----------------------
 
 # run 35 still missing, also re-check mapping files
-tab[1]='/Users/paul/Documents/CU_Pearl_Harbour/Zenodo/Manifest/05_metadata.tsv'
-tab[2]='/Users/paul/Documents/CU_RT_AN/Zenodo/Manifest/10_18S_mapping_file_10410623.tsv'
-tab[3]='/Users/paul/Documents/CU_SP_AD_CH/Zenodo/Manifest/005_metadata_26.tsv'
-tab[4]='/Users/paul/Documents/CU_SP_AD_CH/Zenodo/Manifest/005_metadata_29.tsv'
-tab[5]='/Users/paul/Documents/CU_SP_AD_CH/Zenodo/Manifest/005_metadata_34.tsv'
-tab[6]='/Users/paul/Documents/CU_US_ports_a/Zenodo/Manifest/05_18S_merged_metadata.tsv'
+tab[1]='/Users/paul/Documents/CU_Pearl_Harbour/Zenodo/Manifest/05_metadata.tsv'             # bf76840a9a267058d2d9a1bff27c6787
+tab[2]='/Users/paul/Documents/CU_RT_AN/Zenodo/Manifest/10_18S_mapping_file_10410623.tsv'    # eb22141bc414b4e3782688b1da04760e
+tab[3]='/Users/paul/Documents/CU_SP_AD_CH/Zenodo/Manifest/005_metadata_26.tsv'              # f5fdb49fb8944f26a22e8e55a670b0c7
+tab[4]='/Users/paul/Documents/CU_SP_AD_CH/Zenodo/Manifest/005_metadata_29.tsv'              # 701c19009b5765a49abd1d7a97db5103
+tab[5]='/Users/paul/Documents/CU_SP_AD_CH/Zenodo/Manifest/005_metadata_34.tsv'              # 096040394156041a5316aff9457c9b3e
+tab[6]='/Users/paul/Documents/CU_US_ports_a/Zenodo/Manifest/05_18S_merged_metadata.tsv'     # ba096cf526b694f540e090fb4c5f7d1e
 
 # define output locations
 # -----------------------
